4fa02c00171a0c10c44fcefd9860da1568866ccc
FFYzz <cryptochen95@gmail.com>
2025-08-05 11:56:26 +0800

    Update mt-script.js

--- mt-script.js
+++ mt-script.js
@@ -1,9 +1,10 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj['data']['sku_inventorys'][0]['total_inventory'] = 1;
-body = JSON.stringify(obj);
-
-console.log(body);
-
-$done(body);
+console.log($response);
+if ($response && $response.body) {
+    var obj = JSON.parse(body);
+    obj['data']['sku_inventorys'][0]['total_inventory'] = 1;
+    body = JSON.stringify(obj);
+    console.log(body);
+    $done({ body: body });
+} else {
+    $done({});
+}
