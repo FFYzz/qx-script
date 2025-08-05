0cf3c85c5ab510ce766f49462796dad541c794c8
FFYzz <cryptochen95@gmail.com>
2025-08-05 12:21:00 +0800

    Update mt-script.js

--- mt-script.js
+++ mt-script.js
@@ -1,6 +1,7 @@
 console.log("match script");
 if ($response && $response.body) {
     console.log("match script: response body exist");
+    body = $response.body
     var obj = JSON.parse(body);
     obj['data']['sku_inventorys'][0]['total_inventory'] = 1;
     body = JSON.stringify(obj);
